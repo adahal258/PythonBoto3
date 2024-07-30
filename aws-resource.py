223a37581b8dff321347b4ed0e97c6b060f4aa29
Aashutosh Dahal <AaDahal@health.unm.edu>
2024-07-30 14:15:18 -0600

    Updated functionality with start, stop instance as per user input

--- aws-resource.py
+++ aws-resource.py
@@ -11,25 +11,85 @@ List aws resources: S3 bucket, IAM user, billing, lambda...
 
 '''
 import boto3 as bt
-
-aws_ec2 = bt.client('ec2')
-aws_ec2_instances_info = aws_ec2.describe_instances()
-aws_ec2_reservations = aws_ec2_instances_info.get('Reservations')
+import time
 
 
-def aws_ami_info():
+def ec2_status():
+    aws_ec2 = bt.client('ec2')
+    aws_ec2_instances_info = aws_ec2.describe_instances()
+    aws_ec2_reservations = aws_ec2_instances_info.get('Reservations')
     instance_count = 0
-    print('Your cloud is running following instances: ')
+    print('Your cloud have following instances: ')
     for i in range(0, len(aws_ec2_reservations)):
         aws_ec2_availabe_instance = aws_ec2_reservations[i].get('Instances')
         for j in range(0, len(aws_ec2_availabe_instance)):
             print('########################################################')
-            print(str(instance_count+1) + '. ' + str(aws_ec2_availabe_instance[j].get('InstanceId')))
-            print(f'\t Arch: {aws_ec2_availabe_instance[j].get('InstanceType')} ')
+            aws_ec2_ins = aws_ec2_availabe_instance[j]
+            print(str(instance_count+1) + '. ' + str(aws_ec2_ins.get('InstanceId')))
+            print(f'\t Arch: {aws_ec2_ins.get('InstanceType')} ')
+            if((aws_ec2_ins.get('State')).get('Name')=='running'):
+                print('Status: Running')
+            else:
+                print(str((aws_ec2_ins.get('State')).get('Name')))
             print(f'########################################################\n\n')
             instance_count += 1
+    return aws_ec2
+            
+def aws_ami_info():
+    aws_ec2 = ec2_status()
+    user_input = input("Do you want to perform an action[Y/N]? ")
+    while (user_input !='Y' and user_input != 'N'):
+        print("Please press Y or N")
+        user_input = input("Do you want to perform an action[Y/N]? ")
+    if user_input=='Y':
+        aws_compute_action(aws_ec2)
+    else:
+        print('Thank You!')
+        time.sleep(3)
+        ec2_status()
+        
+def aws_compute_action(aws_ec2):
+    user_action = input("Do you want to start (s), stop(S), Terminate(T) or Exit(E) ? ")
+    while (user_action != 's' and user_action !='S' and user_action != 'T' and user_action != 'E'):
+        print("Accepted: S, T or E")
+        user_action = input("Do you want to start(s), stop(S), Terminate(T) or Exit(E) ? ")
+    if user_action == 's':
+        start_the_instance(aws_ec2)
+    elif user_action == 'S':
+        stop_the_instance(aws_ec2)
+    # elif user_action == 'T':
+    #     terminate_the_instance()
+    else:
+        print("Exiting...........")
+        time.sleep(2)
+        ec2_status()
+        
+
+def start_the_instance(aws_ec2):
+    instance_id = []
+    ins_num = int(input('How many instance you want to start? '))
+    for i in range(0, ins_num):
+        instance_id_info = input('Enter instance id (Please verify it): ')
+        instance_id.append(instance_id_info)
+    print(instance_id)
+    aws_ec2.start_instances(InstanceIds= instance_id)
+    print("\n\nStarting........................\n\n")
+    time.sleep(5)
+    aws_ami_info()
+        
+def stop_the_instance(aws_ec2): 
+    instance_id = []
+    ins_num = int(input('How many instance you want to stop? '))
+    for i in range(0, ins_num):
+        instance_id_info = input('Enter instance id (Please verify it): ')
+        instance_id.append(instance_id_info)
+    print(instance_id)
+    aws_ec2.stop_instances(InstanceIds = instance_id)
+    print("\n\nStopping........................\n\n")
+    time.sleep(5)
+    aws_ami_info()
     
-# def aws_compute_info():
+# def terminate_the_instance():     
 
 #     print(aws_ec2_instances_info)
     
