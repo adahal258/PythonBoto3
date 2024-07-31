89ac8a5ab40016022b9e17774edd25e986ab5ae4
Aashutosh Dahal <AaDahal@health.unm.edu>
2024-07-31 15:32:43 -0600

    Updated try except block and then add terminate feature

--- aws-resource.py
+++ aws-resource.py
@@ -13,7 +13,7 @@ List aws resources: S3 bucket, IAM user, billing, lambda...
 import boto3 as bt
 import time
 
-
+instance_ids = []
 def ec2_status():
     aws_ec2 = bt.client('ec2')
     aws_ec2_instances_info = aws_ec2.describe_instances()
@@ -21,14 +21,18 @@ def ec2_status():
     instance_count = 0
     print('Your cloud have following instances: ')
     for i in range(0, len(aws_ec2_reservations)):
-        aws_ec2_availabe_instance = aws_ec2_reservations[i].get('Instances')
-        for j in range(0, len(aws_ec2_availabe_instance)):
+        aws_ec2_available_instance = aws_ec2_reservations[i].get('Instances')
+        for j in range(0, len(aws_ec2_available_instance)):
             print('########################################################')
-            aws_ec2_ins = aws_ec2_availabe_instance[j]
-            print(str(instance_count+1) + '. ' + str(aws_ec2_ins.get('InstanceId')))
+            aws_ec2_ins = aws_ec2_available_instance[j]
+            aws_ec2_instanceId = aws_ec2_ins.get('InstanceId')
+            instance_ids.append(aws_ec2_instanceId)
+            print(str(instance_count+1) + '. '+ str(aws_ec2_ins.get("Tags")[0].get("Value")) + ":   " + str(aws_ec2_instanceId))
+            print(str())
             print(f'\t Arch: {aws_ec2_ins.get('InstanceType')} ')
             if((aws_ec2_ins.get('State')).get('Name')=='running'):
                 print('Status: Running')
+                print(f'Public Ip: {aws_ec2_ins.get("PublicIpAddress")} Private Ip: {aws_ec2_ins.get("PrivateIpAddress")}')
             else:
                 print(str((aws_ec2_ins.get('State')).get('Name')))
             print(f'########################################################\n\n')
@@ -51,14 +55,14 @@ def aws_ami_info():
 def aws_compute_action(aws_ec2):
     user_action = input("Do you want to start (s), stop(S), Terminate(T) or Exit(E) ? ")
     while (user_action != 's' and user_action !='S' and user_action != 'T' and user_action != 'E'):
-        print("Accepted: S, T or E")
+        print("Accepted: s, S, T or E")
         user_action = input("Do you want to start(s), stop(S), Terminate(T) or Exit(E) ? ")
     if user_action == 's':
         start_the_instance(aws_ec2)
     elif user_action == 'S':
         stop_the_instance(aws_ec2)
-    # elif user_action == 'T':
-    #     terminate_the_instance()
+    elif user_action == 'T':
+        terminate_the_instance(aws_ec2)
     else:
         print("Exiting...........")
         time.sleep(2)
@@ -67,10 +71,26 @@ def aws_compute_action(aws_ec2):
 
 def start_the_instance(aws_ec2):
     instance_id = []
-    ins_num = int(input('How many instance you want to start? '))
+    while True:
+        try:
+            ins_num = int(input('How many instance you want to start? '))
+            break
+        except ValueError:
+            print("The input is not string ONLY NUMBERS: ")
+            continue 
+        
+    if(ins_num > len(instance_ids)):
+        print(f"You only have {len(instance_ids)} in your cloud. ")
+        print(f"Assuming you want to start {len(instance_ids)} instances")
+        ins_num = len(instance_id)
+        
     for i in range(0, ins_num):
         instance_id_info = input('Enter instance id (Please verify it): ')
-        instance_id.append(instance_id_info)
+        if instance_id_info in instance_ids:
+            instance_id.append(instance_id_info)
+        else:
+            print("Please verify instance-id correctly")
+            aws_ami_info()
     print(instance_id)
     aws_ec2.start_instances(InstanceIds= instance_id)
     print("\n\nStarting........................\n\n")
@@ -79,25 +99,60 @@ def start_the_instance(aws_ec2):
         
 def stop_the_instance(aws_ec2): 
     instance_id = []
-    ins_num = int(input('How many instance you want to stop? '))
+    while True:
+        try:
+            ins_num = int(input('How many instance you want to stop? '))
+            break
+        except ValueError:
+            print("The input is not string ONLY NUMBERS: ")
+            continue 
+
+    if(ins_num > len(instance_ids)):
+        print(f"You only have {len(instance_ids)} in your cloud. ")
+        print(f"Assuming you want to stop {len(instance_ids)} instances")
+        ins_num = len(instance_id)
+        
     for i in range(0, ins_num):
         instance_id_info = input('Enter instance id (Please verify it): ')
-        instance_id.append(instance_id_info)
+        if instance_id_info in instance_ids:
+            instance_id.append(instance_id_info)
+        else:
+            print("Please verify instance-id correctly")
+            aws_ami_info()
     print(instance_id)
     aws_ec2.stop_instances(InstanceIds = instance_id)
     print("\n\nStopping........................\n\n")
     time.sleep(5)
     aws_ami_info()
     
-# def terminate_the_instance():     
-
-#     print(aws_ec2_instances_info)
-    
-#     print(f'There are {len(aws_ec2_reservations)} types of virtual server')
-#     print('Here are the list: ')
-#     # for i in range (0, len(aws_ec2_instances_info)):
+def terminate_the_instance(aws_ec2):     
+    instance_id = []
+    while True:
+        try:
+            ins_num = int(input('How many instance you want to terminate? '))
+            break
+        except ValueError:
+            print("The input is not string ONLY NUMBERS: ")
+            continue 
+    if(ins_num > len(instance_ids)):
+        print(f"You only have {len(instance_ids)} in your cloud. ")
+        print(f"Assuming you want to terminate {len(instance_ids)} instances")
+        ins_num = len(instance_id)
         
+    for i in range(0, ins_num):
+        instance_id_info = input('Enter instance id (Please verify it): ')
+        if instance_id_info in instance_ids:
+            instance_id.append(instance_id_info)
+        else:
+            print("Please verify instance-id correctly")
+            aws_ami_info()
+    print(instance_id)
+    aws_ec2.terminate_instances(InstanceIds = instance_id)
+    print("\n\nTerminating........................\n\n")
+    time.sleep(5)
+    aws_ami_info()
 
 if __name__ == '__main__':
     aws_ami_info()
  
+
